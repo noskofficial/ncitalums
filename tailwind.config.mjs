099e29dd7e9a521d98274c4adfff76e48f790cdf
Anish K. Neupane <anishnep51@gmail.com>
2024-10-15 11:17:24 +0545

    fix: whitespaces below and on the right side of /profiles
    Fixed the whitespaces appearing below and on the right side of /profiles due to the size of components. Also, added a custom scrollbar for the menu in /profiles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		plugin(({ addBase, theme }) => {
+			addBase({
+				'.scrollbar': {
+					overflowY: 'auto',
+					scrollbarColor: `${theme('colors.gray.900')} ${theme('colors.gray.800')}`,
+					scrollbarWidth: 'auto',
+				},
+				'.scrollbar::-webkit-scrollbar': {
+					height: '4px',
+					width: '4px',
+				},
+				'.scrollbar::-webkit-scrollbar-thumb': {
+					backgroundColor: theme('colors.gray.900'),
+				},
+				'.scrollbar::-webkit-scrollbar-track-piece': {
+					backgroundColor: theme('colors.gray.800'),
+				},
+			});
+		}),
+	],
 }
