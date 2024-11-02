7110541a0c12a3b408e95b90d1a072beddc332ed
achyut koirala <aoogleak@gmail.com>
2024-11-02 11:38:43 +0545

    Upcoming events section added! (#21)
    * feat: upcoming events section
    
    * fix: cta bg and events fetch from official repo events.json file
    
    * fix: changed the events url to be imported from config
    
    * fix: project structure change in semicolon at eol

--- src/utils/loadconfig.ts
+++ src/utils/loadconfig.ts
@@ -1,12 +1,22 @@
-import fs from "fs";
-import path from "path";
-import yaml from "js-yaml";
-import { getCollection } from 'astro:content';
+import fs from "fs"
+import path from "path"
+import yaml from "js-yaml"
+import { getCollection } from "astro:content"
 
 export interface Root {
   menu: Menu
   featured: FeaturedDetails
   footer: Footer
+  events: Event
+}
+
+export interface Event {
+  upcoming: Upcoming
+}
+
+export interface Upcoming {
+  title: string
+  link: string
 }
 
 export interface Menu {
@@ -35,19 +45,19 @@ export interface SubMenu {
 }
 
 interface FeaturedDetails {
-  profiles: string[];
-  blogs: string[];
+  profiles: string[]
+  blogs: string[]
 }
 
 export function loadConfig(): Root {
-  const filePath = path.resolve("./config.yml");
-  const fileContents = fs.readFileSync(filePath, "utf8");
-  return yaml.load(fileContents) as Root;
+  const filePath = path.resolve("./config.yml")
+  const fileContents = fs.readFileSync(filePath, "utf8")
+  return yaml.load(fileContents) as Root
 }
 
 export const getFeaturedProfiles = async (paths: string[]) => {
-  const allProfiles = await getCollection('profiles');
-  
+  const allProfiles = await getCollection("profiles")
+
   // Filter profiles based on featured.profiles in the config.yml file
-  return allProfiles.filter(profile => paths.includes(profile.slug));
-};
+  return allProfiles.filter((profile) => paths.includes(profile.slug))
+}
